f2cab535b8d34cd444bfaad5f05317a7deb25ff0
MauricioNC <42758875+MauricioNC@users.noreply.github.com>
2018-08-28 10:06:13 -0500

    Update Numero5Digitos.cpp

--- Numero5Digitos.cpp
+++ Numero5Digitos.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
 int main ()
 {
 	int num1;
 	
-	do
-	{
-		system("cls");
-		cout << "\tNumero de intentos: ";
-		cout << "\n\tIngresa un numero de 5 digitos: ";
-		cin >> num1 ;
 	
-		if (num1 >= 10000)
-		{
-			cout << "\n\t" << num1 / 10000 << "   " << num1 % 10000 / 1000 << "   " << num1 % 1000 / 100 << "   " << num1 % 100 / 10 << "   " << num1 % 10 << endl;
-		}
-		else
-		{
-			cout << "\n\tIngresa solamente numeros de 5 cifras";
-			getch();
-		}
+	cout << "\n\tIngresa un numero de 5 digitos: ";
+	cin >> num1 ;
+	
+	if (num1 >= 10000)
+	{
+		cout << "\n\t" << num1 / 10000 << "   " << num1 % 10000 / 1000 << "   " << num1 % 1000 / 100 << "   " << num1 % 100 / 10 << "   " << num1 % 10 << endl;
 	}
-	while(num1 < 10000);
+	else
+	{
+		cout << "\n\tIngresa solamente numeros de 5 cifras";
+	}
 	
 	return 0;
 }
